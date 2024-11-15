fa84097f5c320f1682748d4c55abf22831234a45
Mitsy Prada <mitsy.prada@gmail.com>
2024-11-15 16:03:48 +0100

    Send iput data according to curl
    New functions in ingest.py to send the input data according to the provided curl (described in Mapping API Cribl for Input API). Run test.py to do the Input API call

--- ingest.py
+++ ingest.py
@@ -1,13 +1,15 @@
-from pydantic import BaseModel
-from schemas import AuthenticationSchema, ConnectionSchema, InputSchema, Metadata, Schedule, Collector, CollectorConf, Run, InputType
-import tomli
 import re
-from rest_utilities import get_cribl_authentication_token, post_new_database_connection, post_new_input
 from uuid import uuid4
-from typing import List, Tuple, Dict, Any
+from typing import List
+
+import tomli
+from pydantic import BaseModel
+
+from rest_utilities import get_cribl_authentication_token, post_new_database_connection, post_new_input
+from schemas import InputSchema, Metadata, Schedule, Collector, CollectorConf, Run, InputType
 
 
-def load_input_examples(files: List[str]) -> tuple[object, object]:
+def load_examples(files: List[str]) -> tuple[object, object]:
     """
     Load and parse two TOML files, returning the contents as dictionaries.
 
@@ -22,64 +24,49 @@ def load_input_examples(files: List[str]) -> tuple[object, object]:
         A tuple containing two dictionaries with the parsed data from each TOML file.
     """
 
-    def load_and_format_db_inputs_toml(file_path: str) -> object:
+    def load_and_format(file_path: str) -> object:
         with open(file_path) as f:
             tmp = f.read()
-            transformed_lines = []
-            multiline_query = False
-            for line in tmp.split('\n'):
-                line = re.sub(r"\s*=\s*", " = ", line)
-                if 'query' in line:
-                    if '\\' in line:
-                        query_content = line.split('query = ')[1].rstrip("\\").strip()
-                        # Append the modified 'query' line with triple quotes
-                        transformed_lines.append("query = '''")
-                        # Append the original content on a new line
-                        transformed_lines.append(query_content)
-                        multiline_query = True
-                    else:
-                        # Add other lines as they are
-                        transformed_lines.append(line)
-                        multiline_query = False
-
-                elif multiline_query == True and '\\' in line:
-                    transformed_lines.append(line)
-                    multiline_query = True
-
-                elif multiline_query == True and '\\' not in line:
-                    transformed_lines.append(line)
-                    transformed_lines.append("'''")
-                    multiline_query = False
-                else:
-                    transformed_lines.append(line)
-
-        transformed_text = "\n".join(transformed_lines)
-        text = transformed_text.strip()
-        # Wrap the text with triple double quotes, starting and ending on a new line
-        formatted_text = f"""\n{text}\n"""
-        tmp = re.sub(r"\\\n", "\n", formatted_text)
-        tmp = re.sub(r"(\s*=\s*)(.*)", r"\1'\2'", tmp)
-        tomli_db_inputs = tomli.loads(tmp).items()
+            tmp = re.sub(r"\\\n", " ", tmp)
+            tmp_formatted = re.sub(r"(\s*=\s*)(.*)", r"\1'''\2'''", tmp)
+            tomli_db_inputs = tomli.loads(tmp_formatted).items()
         return tomli_db_inputs
 
-    def load_and_format_db_connections(file_path: str) -> object:
-        with open(file_path) as f:
-            tmp = f.read()
-            tmp_formatted = re.sub(r"(\s*=\s*)(.*)", r"\1'\2'", tmp)
-            tomli_db_connections = tomli.loads(tmp_formatted).items()
-        return tomli_db_connections
 
     # Load and parse each file
-    dict_items1 = load_and_format_db_inputs_toml(files[0])
-    dict_items2 = load_and_format_db_connections(files[1])
+    dict_items1 = load_and_format(files[0])
+    dict_items2 = load_and_format(files[1])
 
     return dict_items1, dict_items2
 
 
+def is_cron_format(value: str) -> bool:
+    """
+    Check if the given value is in a valid cron schedule format.
+
+    Parameters
+    ----------
+    value : str
+        The value to check.
+
+    Returns
+    -------
+    bool
+        True if the value is a valid cron schedule format, False otherwise.
+    """
+    if value.isdigit():
+        return False
+
+    # Cron schedule regex: Matches standard cron patterns
+    cron_regex = r'^(\*|([0-5]?\d)) (\*|([01]?\d|2[0-3])) (\*|([0-2]?\d)) (\*|([1-7]))$|^@(?:yearly|annually|monthly|weekly|daily|hourly)$'
+
+    return bool(re.match(cron_regex, value.strip()))
+
+
 class Ingestor:
     def __init__(
-        self,
-        examples_folder: str = "examples",
+            self,
+            examples_folder: str = "examples",
     ):
         self.examples_folder = examples_folder
         self.identities = None
@@ -92,8 +79,21 @@ class Ingestor:
     def get_cribl_authtoken(self, base_url: str = "http://localhost:19000") -> str:
         self.token = get_cribl_authentication_token(base_url=base_url)
 
+    def merge_examples_input(self, file_names: list | None = None) -> object:
+        """
 
-    def merge_examples_input(self, file_names: list | None = None):
+        Parameters
+        ----------
+        file_names : list
+            A list containing two file names to be merged. The first file should contain the input data, and the second file should contain the connection data.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the merged data from the two files.
+
+        """
+
         if file_names and file_names[0]:
             path1 = f"{self.examples_folder}/{file_names[0]}"
         else:
@@ -105,7 +105,7 @@ class Ingestor:
 
         paths = [path1, path2]
 
-        tomli_db_inputs, tomli_db_connections = load_input_examples(files=paths)
+        tomli_db_inputs, tomli_db_connections = load_examples(files=paths)
 
         merged_data = {}
         dict_db_inputs = dict(tomli_db_inputs)
@@ -116,8 +116,10 @@ class Ingestor:
             connection_data = dict_db_connections.get(connection_key, {})
             merged_data[key] = {**input_value, **connection_data}
 
-        # Transform values in "disabled" entry
         def transform_disabled_values(data: dict):
+            """
+            Transform values in "disabled" entry according to provided table in Mapping API Cribl
+            """
             for key, sub_dict in data.items():
                 if 'disabled' in sub_dict:
                     value = sub_dict['disabled']
@@ -128,23 +130,82 @@ class Ingestor:
                     elif value in ['false', 0, False, '0']:
                         sub_dict['disabled'] = True
             return data
+        def seconds_to_cron(seconds: int) -> str:
+            """
+            Transforms a given time in seconds to a cron schedule.
+
+            Parameters
+            ----------
+            seconds : int
+                The time in seconds to be converted to a cron schedule.
+
+            Returns
+            -------
+            str
+                The cron schedule expression.
+            """
+            if seconds < 60:
+                return f"*/{seconds} * * * *"
+            elif seconds < 3600:
+                minutes = seconds // 60
+                return f"*/{minutes} * * * *"
+            elif seconds < 86400:
+                hours = seconds // 3600
+                return f"0 */{hours} * * *"
+            else:
+                days = seconds // 86400
+                return f"0 0 */{days} * *"
 
         merged_input_data = transform_disabled_values(merged_data).items()
+
+        for key, sub_dict in merged_input_data:
+            interval = sub_dict.get('interval')
+            if interval and not is_cron_format(interval):
+                try:
+                    sub_dict['interval'] = seconds_to_cron(int(interval))
+                except ValueError:
+                    continue
 
         return merged_input_data
 
     def load_input(self, file_names: list | None = None) -> list[BaseModel] | None:
+        """
+        Load the input examples from the TOML files and create InputSchema instances.
+
+        Parameters
+        ----------
+        file_names: list
+            A list containing two file names to be merged. The first file should contain the input data, and the second file should contain the connection data.
+
+        Returns
+        -------
+        List[InputSchema]
+            A list containing the InputSchema instances.
+
+        """
         def create_metadata(data):
+            """
+            Create a list of Metadata instances from the given data.
+
+            Parameters
+            ----------
+            data
+
+            Returns
+            -------
+
+
+            """
             return [
-                Metadata(name="host", value=data["host"]),
-                Metadata(name="index", value=data["index"]),
+                Metadata(name="host", value=data.get("host","")),
+                Metadata(name="index", value=data.get("index","")),
                 Metadata(name="source", value=data.get("source", "")),
-                Metadata(name="sourcetype", value=data["sourcetype"])
+                Metadata(name="sourcetype", value=data.get("sourcetype",""))
             ]
 
         merged_data = self.merge_examples_input(file_names)
         # Parsing tomli_input to create InputSchema instances
-        self.input = [
+        input_obj = [
             InputSchema(
                 schedule=Schedule(
                     interval=row['interval'],
@@ -165,50 +226,29 @@ class Ingestor:
             for key, row in merged_data
         ]
 
+
+        def transform_to_js_expression(query: str) -> str:
+            """
+            Transforms the SQL query strings into a valid JavaScript expression
+            for Cribl, enclosed in backticks.
+            """
+            # Escape double quotes for JavaScript compatibility
+            escaped_query = query.replace('"', '\\"')
+            # Wrap the query in backticks
+            js_expression = f"`{escaped_query}`"
+            return js_expression
+
+        for i in input_obj:
+            i.collector.conf.query = transform_to_js_expression(i.collector.conf.query)
+
+        self.input = input_obj
+
         return self.input
 
-
-
-
-
-    def load_identities(
-        self, file_name: str | None = None
-    ) -> list[BaseModel] | None:
-        if file_name:
-            path = f"{self.examples_folder}/{file_name}"
-        else:
-            path = f"{self.examples_folder}/identities.conf"
-        self.identities = load_input_examples()
-        return self.identities
-
-    def load_connection(self, file_name: str | None = None) -> list[BaseModel] | None:
-        if file_name:
-            path = f"{self.examples_folder}/{file_name}"
-        else:
-            path = f"{self.examples_folder}/db_connections.conf"
-        self.connection = load_input_examples()
-        return self.connection
-
-
-    def post_db_connections(
-        self,
-        base_url: str = "http://localhost:19000",
-        cribl_workergroup_name: str = "default",
-    ) -> list[dict]:
-        return [
-            post_new_database_connection(
-                base_url=base_url,
-                cribl_authtoken=self.token,
-                cribl_workergroup_name=cribl_workergroup_name,
-                payload=i.model_dump_json(),
-            )
-            for i in self.connection
-        ]
-
     def post_db_inputs(
-        self,
-        base_url: str = "http://localhost:19000",
-        cribl_workergroup_name: str = "default",
+            self,
+            base_url: str = "http://localhost:19000",
+            cribl_workergroup_name: str = "default",
     ) -> list[dict]:
         return [
             post_new_input(
@@ -219,4 +259,18 @@ class Ingestor:
             )
             for i in self.input
         ]
-        
+
+    def post_db_connections(
+            self,
+            base_url: str = "http://localhost:19000",
+            cribl_workergroup_name: str = "default",
+    ) -> list[dict]:
+        return [
+            post_new_database_connection(
+                base_url=base_url,
+                cribl_authtoken=self.token,
+                cribl_workergroup_name=cribl_workergroup_name,
+                payload=i.model_dump_json(),
+            )
+            for i in self.connection
+        ]
